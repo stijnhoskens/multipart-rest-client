e931fe4e51b793dde1238510a99ec83a215eb92d
Stijn Hoskens <stijn.hoskens@ubench.com>
2021-04-17 14:17:29 +0200

    Setup unsuccesful test

--- src/main/java/org/acme/rest/client/multipart/server/EchoService.java
+++ src/main/java/org/acme/rest/client/multipart/server/EchoService.java
@@ -1,5 +1,9 @@
 package org.acme.rest.client.multipart.server;
 
+import org.acme.rest.client.multipart.MultipartBody;
+import org.acme.rest.client.multipart.MultipartService;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -7,12 +11,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/echo")
-public class EchoService {
+public class EchoService implements MultipartService {
 
+    @Override
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.TEXT_PLAIN)
-    public String echo(String requestBody) throws Exception {
-        return requestBody;
+    public String sendMultipartData(@MultipartForm MultipartBody data) {
+        return String.format("Successfully uploaded %s", data.fileName);
     }
 }
--- src/test/java/org/acme/rest/client/multipart/MultipartResourceTest.java
+++ src/test/java/org/acme/rest/client/multipart/MultipartResourceTest.java
@@ -2,17 +2,14 @@ package org.acme.rest.client.multipart;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.junit.jupiter.api.Test;
 
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.ByteArrayInputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
 public class MultipartResourceTest {
@@ -25,9 +22,10 @@ public class MultipartResourceTest {
         MultipartBody multipartBody = new MultipartBody();
         multipartBody.fileName = "greeting.txt";
         multipartBody.file = new ByteArrayInputStream("HELLO WORLD".getBytes(StandardCharsets.UTF_8));
-        Response response = ClientBuilder.newClient().target(uri)
-                .request().post(Entity.entity(multipartBody, MediaType.MULTIPART_FORM_DATA_TYPE));
-        assertEquals(200, response.getStatus());
+        MultipartService multipartService =
+                RestClientBuilder.newBuilder().baseUri(uri)
+                        .build(MultipartService.class);
+        assertNotNull(multipartService.sendMultipartData(multipartBody));
     }
 
 }
